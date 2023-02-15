7e0ab9939f84b54b621c4c1a39bffbba83e1b27d
varun sai <varunrockz202000@gmail.com>
2023-02-15 11:53:29 +0000

    Update addProfilePic.js

--- src/pages/Profile/fileUpload/addProfilePic.js
+++ src/pages/Profile/fileUpload/addProfilePic.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import React from "react";
 export default function AddProfilePic({ open, onClose }) {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [profileImage,setProfileImage]=useState([]);
+  const [profileImage, setProfileImage] = useState([]);
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
@@ -13,10 +13,9 @@ export default function AddProfilePic({ open, onClose }) {
   const handleSubmit = () => {
     const reader = new FileReader();
     const id = localStorage.getItem("UserId");
-    
+
     if (selectedFile == null) {
       onClose();
-      // window.location.reload();
     } else {
       reader.readAsDataURL(selectedFile);
 
@@ -27,19 +26,20 @@ export default function AddProfilePic({ open, onClose }) {
         );
         console.log(imageData);
         setProfileImage(imageData);
-        // window.location.reload();
-         fetch('https://c5rbbler50.execute-api.us-east-1.amazonaws.com/v4/save-profile-inno', {
-        method: 'POST',
-        body: JSON.stringify({ image: imageData, UserId:id }),
-      })
-        .then((response) => response.json())
-        // .then((data) => {
-        //   // Handle the response from the Lambda function
-        // });
+
+        fetch(
+          "https://c5rbbler50.execute-api.us-east-1.amazonaws.com/v4/save-profile-inno",
+          {
+            method: "POST",
+            body: JSON.stringify({ image: imageData, UserId: id }),
+          }
+        ).then((response) => response.json());
+        window.location.reload();
       };
+      alert("Image Updated and Profile picture will update soon");
     }
   };
-  
+
   return (
     <>
       <div className="add-profilepic-body"> </div>
