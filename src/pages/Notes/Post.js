0124c85e69f2dd398266a3ee413891b8d8bffc4a
varun sai <varunrockz202000@gmail.com>
2023-02-27 17:23:40 +0000

    Update 8 files

--- src/pages/Notes/Post.js
+++ src/pages/Notes/Post.js
@@ -6,8 +6,8 @@ import * as MdIcons from "react-icons/md";
 const Post = ({ post }) => {
   return (
     <>
-      <div className="saved">
-        <div className="saved-note">
+      <div className="NotesPage">
+        <div className="NotesPage-note">
           <div className="note-header" key={post.DocumentID}>
             <a id="saved-title" href={post.UrlId} target="_blank" >
               {post.Title}
